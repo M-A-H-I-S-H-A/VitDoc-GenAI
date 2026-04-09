d61db43d6d6ee91594ccc47429092481d70b0520
Mahisha Parameshwari NM <231801094@rajalakshmi.edu.in>
2026-04-09 23:08:39 +0530

    final working version

--- server.js
+++ server.js
@@ -36,7 +36,7 @@ app.post("/chat", async (req, res) => {
     const { message, profile } = req.body;
 
     const response = await fetch(
-      "https://generativelanguage.googleapis.com/v1beta/models/gemini-pro:generateContent?key=AIzaSyBC5dezniWbynnCJKfqCdmd-LRpzIr_XBA",
+      `https://generativelanguage.googleapis.com/v1beta/models/gemini-pro:generateContent?key=${process.env.GEMINI_API_KEY}`
       {
         method: "POST",
         headers: {
