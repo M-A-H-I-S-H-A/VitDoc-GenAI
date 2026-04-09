fd5d9c0f54f2530ef53a2bca0ba839cc47af71c4
Mahisha Parameshwari NM <231801094@rajalakshmi.edu.in>
2026-04-09 22:10:15 +0530

    fixed port for railway

--- server.js
+++ server.js
@@ -70,5 +70,5 @@ app.post("/chat", async (req, res) => {
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
+  console.log(`Server running on port:${PORT}`);
 });
